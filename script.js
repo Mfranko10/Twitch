cb97e7ef1be85c3dfc00c85d96d61bdc12ef27f2
Miguel Franco <98226415+Mfranko10@users.noreply.github.com>
2024-11-02 23:24:26 -0500

    Update script.js
    dsfdsdaf

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 {
-  Math.floor(Math.random() * 100) 
+  var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://frasedeldia.azurewebsites.net/api/phrase');
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        console.log(xhr.responseText);
+    }
+    else {
+        console.log('Error');
+    }
+};
+xhr.send();
 }
