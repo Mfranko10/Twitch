8ac3828b678bac0180ada42c1b6c19e201816c82
Miguel Franco <98226415+Mfranko10@users.noreply.github.com>
2024-11-03 18:20:21 -0500

    Update script.js
    fdgdfgsdf

--- script.js
+++ script.js
@@ -5,4 +5,6 @@
     "No te rindas, el principio siempre es lo más difícil.",
     "El éxito no es la clave de la felicidad; la felicidad es la clave del éxito.",
     "No puedes cambiar lo que está fuera de ti, pero puedes cambiar lo que está dentro."];
+
+	frases[Math.floor(Math.random() * frases.lenght)]
 }
