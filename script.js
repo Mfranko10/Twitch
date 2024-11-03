2c2206884caef732a46aa05c1a1e138c8091de24
Miguel Franco <98226415+Mfranko10@users.noreply.github.com>
2024-11-03 18:12:07 -0500

    Update script.js
    asdasd

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 {
-	const frases ={
+	const frases =[
     "La vida es un 10% lo que nos ocurre y un 90% cómo reaccionamos.",
     "No cuentes los días, haz que los días cuenten.",
     "No te rindas, el principio siempre es lo más difícil.",
     "El éxito no es la clave de la felicidad; la felicidad es la clave del éxito.",
-    "No puedes cambiar lo que está fuera de ti, pero puedes cambiar lo que está dentro.",};
-	const n = Math.floor(Math.random() * frases.length);
-    '$(frases.n)'
+    "No puedes cambiar lo que está fuera de ti, pero puedes cambiar lo que está dentro."];
 }
