f9475b7bfc3e115ed06a97f862e4a20fd2d82563
Miguel Franco <98226415+Mfranko10@users.noreply.github.com>
2024-11-02 23:28:22 -0500

    Update script.js
    sadasd

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-{
-  const url = 'https://palavras-aleatorias.p.rapidapi.com/words/5/2';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '71ad27b963mshe2e5032b7150432p188038jsnbf44aff87d28',
-		'x-rapidapi-host': 'palavras-aleatorias.p.rapidapi.com'
-	}
-};
+()=>({
+	const url = 'https://palavras-aleatorias.p.rapidapi.com/words/5/2';
+	const options = {
+		method: 'GET',
+		headers: {
+			'x-rapidapi-key': '71ad27b963mshe2e5032b7150432p188038jsnbf44aff87d28',
+			'x-rapidapi-host': 'palavras-aleatorias.p.rapidapi.com'
+		}
+	};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-}
+	try {
+		const response = await fetch(url, options);
+		const result = await response.text();
+		console.log(result);
+	} catch (error) {
+		console.error(error);
+	}
+})
