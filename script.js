973b82647828d7df4d5e38669b5d3b1c09c528d7
Miguel Franco <98226415+Mfranko10@users.noreply.github.com>
2024-11-03 18:22:17 -0500

    Update script.js
    fgdsg

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-{
+
 	const frases =[
     "La vida es un 10% lo que nos ocurre y un 90% cómo reaccionamos.",
     "No cuentes los días, haz que los días cuenten.",
@@ -6,5 +6,4 @@
     "El éxito no es la clave de la felicidad; la felicidad es la clave del éxito.",
     "No puedes cambiar lo que está fuera de ti, pero puedes cambiar lo que está dentro."];
 
-	frases[Math.floor(Math.random() * frases.lenght)]
-}
+frases[Math.floor(Math.random() * frases.lenght)]
