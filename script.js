543335fa03a2d6ffa8f6245175817bfbee2dbf0b
Miguel Franco <98226415+Mfranko10@users.noreply.github.com>
2024-11-02 23:35:55 -0500

    Update script.js
    adasd

--- script.js
+++ script.js
@@ -4,14 +4,7 @@
     "No cuentes los días, haz que los días cuenten.",
     "No te rindas, el principio siempre es lo más difícil.",
     "El éxito no es la clave de la felicidad; la felicidad es la clave del éxito.",
-    "No puedes cambiar lo que está fuera de ti, pero puedes cambiar lo que está dentro.",
-    // Agrega aquí más frases hasta completar las 1000
-];
-	function obtenerFraseAleatoria() {
+    "No puedes cambiar lo que está fuera de ti, pero puedes cambiar lo que está dentro.",];
     const indiceAleatorio = Math.floor(Math.random() * frases.length);
-    return frases[indiceAleatorio];
-}
-
-// Llamada a la función para mostrar una frase aleatoria
-obtenerFraseAleatoria()
+    frases[indiceAleatorio];
 }
